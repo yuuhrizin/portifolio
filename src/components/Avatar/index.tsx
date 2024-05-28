b74b019919d573adfe00bca10c1fb7db8bd77f1e
Yuri Luiz <yuri.luitzkt@gmail.com>
2024-05-28 12:48:42 -0300

    add full portifolio ebac

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/yuuhlkt.png" />
+
+export default Avatar
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Yuri Luiz</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        yuuhlkt
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Fullstack Java
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
+
+export default Sidebar
